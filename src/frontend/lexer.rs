3079ee3f3ffa107c1864f5f6018fe5eb8654e3a8
Balaji <balajikaredla4@gmail.com>
2026-03-04 17:54:45 +0530

    file reads and lexes

--- src/frontend/lexer.rs
+++ src/frontend/lexer.rs
@@ -4,7 +4,17 @@ pub fn lexer_rules() -> LexerRules {
     santiago::lexer_rules!(
         "DEFAULT" | "INT" = pattern r"[0-9]+";
         "DEFAULT" | "FLOAT" = pattern r"([0-9]+\.)|([0-9]*\.[0-9]+)";
+        "DEFAULT" | "STRING" = pattern r#""([^"\\]|\\.)*""#;
+        "DEFAULT" | "NAME" = pattern r"[a-zA-Z_]*";
         "DEFAULT" | "PLUS" = string "+";
+        "DEFAULT" | "MINUS" = string "-";
+        "DEFAULT" | "MULT" = string "*";
+        "DEFAULT" | "DIV" = string "/";
+        "DEFAULT" | "LEFT_CURLY_BRACKET" = string "{";
+        "DEFAULT" | "RIGHT_CURLY_BRACKET" = string "}";
+        "DEFAULT" | "LEFT_ROUND_BRACKET" = string "(";
+        "DEFAULT" | "RIGHT_ROUND_BRACKET" = string ")";
+        "DEFAULT" | "COMMENT" = pattern "//.*" => |lexer| lexer.skip();
         "DEFAULT" | "WS" = pattern r"\s" => |lexer| lexer.skip();
     )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use rs_clp::frontend::lexer::lexer_rules;
 
 fn main() {
-    let input = "10. + 20.0 + .30";
+    // let input = "10. + 20.0 + .30\n30 - 20";
+
+    let input = std::fs::read_to_string("examples/test1.lc").expect("File error");
 
     let lexer_rules = lexer_rules();
     let lexemes = santiago::lexer::lex(&lexer_rules, &input).expect("Error in lexing");
 
     for lexeme in lexemes {
-        println!("Lexeme: {}, Value: {}", lexeme, lexeme.raw);
+        println!("Lexeme: {}, Value: {}, Line: {}", lexeme, lexeme.raw, lexeme.position.line);
         // println!(" {}", lexeme.raw);
     }
 
