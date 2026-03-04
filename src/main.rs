de310bd36b88d9d11c97ff4525dc4bd271c88055
Balaji <balajikaredla4@gmail.com>
2026-03-04 17:22:07 +0530

    add floats in the language

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use rs_clp::frontend::lexer::lexer_rules;
 
 fn main() {
-    let input = "10 + 20 + 30";
+    let input = "10. + 20.0 + .30";
 
     let lexer_rules = lexer_rules();
-    let lexemes = santiago::lexer::lex(&lexer_rules, &input).unwrap();
+    let lexemes = santiago::lexer::lex(&lexer_rules, &input).expect("Error in lexing");
 
     for lexeme in lexemes {
-        println!("{}", *lexeme);
+        println!("Lexeme: {}, Value: {}", lexeme, lexeme.raw);
+        // println!(" {}", lexeme.raw);
     }
 
     println!("Hello, world!");
